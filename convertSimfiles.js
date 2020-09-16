b7719b783e1186dd7b77af958777bc6e2022fedd
Sean Morrissey <chaosharmonic@outlook.com>
2020-09-16 02:16:18 -0400

    Document conversion script

--- convertSimfiles.js
+++ convertSimfiles.js
@@ -2,13 +2,15 @@ import { walkSync, ensureDirSync } from 'https://deno.land/std@v0.64.0/fs/mod.ts
 import { headerProps, getSectionProps, readSimfile, allArcadeReleases } from './utils.js'
 
 const convertHeader = (inputFile) => {
-  const inputHeader = inputFile.split('\n\n')[0]
-  const inputProps = getSectionProps(inputHeader)
-    .filter(prop => typeof (prop) !== 'string')
+  const inputHeader = inputFile.split('\n\n')[0] // get song metadata
+  const inputProps = getSectionProps(inputHeader) // convert metadata to objects
+    .filter(prop => typeof (prop) !== 'string') // remove blank properties
   const inputVals = inputProps
     .reduce((a, b) => ({ ...a, ...b }))
 
-  const createHeader = (props) => props
+  // concat values to properties
+  //   then, convert properties array to text block
+  const generateHeader = (props) => props
     .map(propName => {
       const value = inputVals[propName] || ''
       const prop = propName.toUpperCase()
@@ -20,8 +22,8 @@ const convertHeader = (inputFile) => {
 
   // minHeader will copy fields with values only
   // fullHeader will copy template.ssc and insert values
-  const minHeader = createHeader(minHeaderProps)
-  // const fullHeader = createHeader(headerProps)
+  const minHeader = generateHeader(minHeaderProps)
+  // const fullHeader = generateHeader(headerProps)
 
   const header = minHeader
 
@@ -37,9 +39,9 @@ const convertCharts = (inputFile) => {
   ]
 
   const rawChartData = inputFile
-    .replace(/,\n\n/g, ',\n')
-    .split('\n\n')
-    .slice(1)
+    .replace(/,\n\n/g, ',\n') // remove double space between measures
+    .split('\n\n') // separate text blocks
+    .slice(1) // filter out song metadata
 
   const chartProps = rawChartData
     .filter(text => !text.includes('0000'))
@@ -47,31 +49,38 @@ const convertCharts = (inputFile) => {
   const chartValues = rawChartData
     .filter(text => text.includes('0000'))
 
-  const hasChartProps = inputFile.includes('STEPSTYPE')
+  const hasChartProps = inputFile.includes('STEPSTYPE') // get input file format
 
   const output = chartProps.map(prop => {
+    // split block into individual lines, stripping out boilerplate
     const propValues = `${prop}\n`
       .replace(/(:|;)\n/g, 'SPLIT')
       .split('SPLIT')
       .filter(line => line && (!line.includes('NOTE')))
 
+    // if SSC-fomratted, get value only
+    //   else, get whole line
     const stepstype = hasChartProps
       ? propValues[0]
         .split(':')[1]
       : propValues[0]
 
+    // if SSC-fomratted, return whole line
+    //   else, concat value to relevant field
     const data = propValues.map((value, index) => hasChartProps
       ? `${value};`
       : `${fields[index]}:${value};`)
 
     const divider =
-                  `//---------------${stepstype}-----------------`
+      `//---------------${stepstype}-----------------`
 
+    // wrap chart metadata w boilerplate lines and divider
     return [divider, '#NOTEDATA:;', ...data, '#NOTES:']
       .join('\n')
       .replace('\n\n', '\n')
   })
 
+  // concat chart metadata and corresponding notes
   return output.map((props, index) => [props, chartValues[index]]
     .join('\n\n'))
 }
@@ -79,10 +88,10 @@ const convertCharts = (inputFile) => {
 const convertSMToSSC = (source) => {
   const sim = readSimfile(source)
     .split('\n')
-    .filter(text => !text.startsWith('//'))
+    .filter(text => !text.startsWith('//')) // remove comment lines
     .map(text => {
-      if (text.includes('//')) text = text.split('//')[0]
-      return text.trim()
+      if (text.includes('//')) text = text.split('//')[0] // strip measure labels
+      return text.trim() // remove leading/trailing blank space
     })
     .join('\n')
 
@@ -92,7 +101,7 @@ const convertSMToSSC = (source) => {
   return [header, ...charts].join('\n\n')
     .split('\n')
     .map(line => line.includes('=')
-      ? line.replace(/,/g, ',\n')
+      ? line.replace(/,/g, ',\n') // one bpm change, stop, etc. per line
       : line)
     .join('\n')
 }
